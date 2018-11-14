e81882aec2da9534e11c6bf0b048d624d04f6bcb
AndPotap <ap3635@columbia.edu>
2018-11-13 20:28:22 -0500

    Included the a function to augment the data at the marathon split level

--- AndPotap/Utils/feature_generation.py
+++ AndPotap/Utils/feature_generation.py
@@ -9,6 +9,7 @@
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Imports
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+import re
 import time
 import pandas as pd
 # ===========================================================================
@@ -21,9 +22,9 @@ def format_correctly(df, verbose=False):
     """
     Properly formats the DateTime objects and introduces inplace
     the DateTime objects columns in seconds
-    :param df: pd.DataFrame | the marathon pd.DataFrame
+    :param df: DataFrame | the marathon DataFrame
     :param verbose: bool | True prints the time the function took
-    :return: pd.DataFrame | the original pd.DataFrame with the
+    :return: DataFrame | the original DataFrame with the
                             extra columns
     """
     t0 = time.time()
@@ -36,6 +37,7 @@ def format_correctly(df, verbose=False):
 
         # Generate new column
         new_col = col + '_sec'
+        # km = int(re.findall(r'\d+', col)[0])
 
         # Introduce the DateTime objects
         aux = pd.DatetimeIndex(df[col])
@@ -47,6 +49,31 @@ def format_correctly(df, verbose=False):
         df.loc[mask, new_col] = (aux[mask].hour * 3600 +
                                  aux[mask].minute * 60 +
                                  aux[mask].second)
+
+    rename_cols = {'splint_5k_sec': 'sec_1',
+                   'splint_10k_sec': 'sec_2',
+                   'splint_15k_sec': 'sec_3',
+                   'splint_20k_sec': 'sec_4',
+                   'splint_25k_sec': 'sec_5',
+                   'splint_30k_sec': 'sec_6',
+                   'splint_35k_sec': 'sec_7',
+                   'splint_40k_sec': 'sec_8'}
+
+    rename_cols_2 = {'splint_5k': '5k_1',
+                     'splint_10k': '10k_2',
+                     'splint_15k': '15k_3',
+                     'splint_20k': '20k_4',
+                     'splint_25k': '25k_5',
+                     'splint_30k': '30k_6',
+                     'splint_35k': '35k_7',
+                     'splint_40k': '40k_8'}
+
+    df = df.rename(columns=rename_cols)
+
+    df = df.rename(columns=rename_cols_2)
+
+    df['ID'] = df.index
+
     t1 = time.time()
     if verbose:
         print('Formatting took: {:6.1f} sec'.format(t1 - t0))
@@ -54,26 +81,53 @@ def format_correctly(df, verbose=False):
     return df
 
 
+def subset_columns(columns):
+    """
+    Subsets a given list of columns to the ones matching
+    the ('\d+') RegEx pattern
+    :param columns: pd.Index | index of a given DataFrame
+    :return: list | a list containing matching elements
+    """
+    subset = []
+    for col in columns:
+        if len(re.findall(r'_\d+', string=col)) > 0:
+            subset.append(col)
+    return subset
+
+
+def give_difference(total_columns, columns):
+    """
+    Returns the difference between two given sets of columns
+    :param total_columns: pd.Index | index of the whole data set
+    :param columns: list | a subset list of columns from total
+    :return: list | the columns not contained in the subset
+    """
+    a = set(total_columns)
+    b = set(columns)
+    c = a.difference(b)
+    return list(sorted(c))
+
+
 def add_diffs(df, verbose=False):
     """
     Adds the features that contain the differences in seconds
     between the marathon 5 k splits
-    :param df: pd.DataFrame | the marathon pd.DataFrame
+    :param df: DataFrame | the marathon DataFrame
     :param verbose: bool | True prints the time the function took
-    :return: pd.DataFrame | the original pd.DataFrame with
+    :return: DataFrame | the original DataFrame with
                             the extra columns
     """
     t0 = time.time()
-    col_diffs = ['splint_5k_sec',
-                 'splint_10k_sec',
-                 'splint_15k_sec',
-                 'splint_20k_sec',
-                 'splint_25k_sec',
-                 'splint_30k_sec',
-                 'splint_35k_sec',
-                 'splint_40k_sec']
+    col_diffs = ['sec_1',
+                 'sec_2',
+                 'sec_3',
+                 'sec_4',
+                 'sec_5',
+                 'sec_6',
+                 'sec_7',
+                 'sec_8']
 
-    df.loc[:, 'pace_total'] = df.loc[:, 'splint_40k_sec'] / len(col_diffs)
+    df.loc[:, 'pace_total'] = df.loc[:, col_diffs[-1]] / len(col_diffs)
 
     df.loc[:, 'diff_1'] = df[col_diffs[0]]
 
@@ -105,41 +159,39 @@ def add_diffs(df, verbose=False):
 
 
 def reshape_properly(df, verbose=False):
+    """
+    Augments the DataFrame by a factor of 8 in order
+    to incorporate the analysis at a marathon split level
+    :param df: DataFrame | the raw marathon data
+    :param verbose: bool | True prints the time the function took
+    :return: DataFrame | the augmented DataFrame
+    """
     t0 = time.time()
-    df['ID'] = df.index
     aux = df.copy()
-    id_vars = ['ID',
-               'bib',
-               'gender_age',
-               'gun_place',
-               'gun_time',
-               'name',
-               'official_time',
-               'pace_per_mile',
-               'percentile_age-graded',
-               'place',
-               'place_age-graded',
-               'place_age-graded_of',
-               'place_age-group',
-               'place_age-group_of',
-               'place_gender',
-               'place_gender_of',
-               'place_overall',
-               'place_overall_of',
-               'team',
-               'time_age-graded',
-               'pace_total',
-               'splint_half']
 
-    # Melt the DataFrame
+    other = subset_columns(columns=df.columns)
+    id_vars = give_difference(total_columns=df.columns,
+                              columns=other)
+
+    # Melt for some columns (this is placeholder)
+    selected = [col for col in df.columns if 'k' in col]
+    df_placeholder = pd.melt(frame=aux, id_vars=id_vars,
+                             value_vars=selected)
+    df_placeholder.loc[:, 'ID_R'] = (df_placeholder['ID'].apply(str) + '_'
+                          + df_placeholder['variable'].str.get(-1))
+
+    # Melt for all the columns
     aux = pd.melt(frame=aux, id_vars=id_vars)
+
+    aux.loc[:, 'ID_R'] = (aux['ID'].apply(str) + '_'
+                          + aux['variable'].str.get(-1))
 
     aux.loc[:, 'identifier'] = 'NaN'
 
     mask = aux['variable'].str.contains('k')
     aux.loc[mask, 'identifier'] = 'date_time'
 
-    mask = aux['variable'].str.contains('k_sec')
+    mask = aux['variable'].str.contains('sec_')
     aux.loc[mask, 'identifier'] = 'sec'
 
     mask = aux['variable'].str.contains('diff_')
@@ -152,16 +204,30 @@ def reshape_properly(df, verbose=False):
     aux.loc[mask, 'identifier'] = 'pace_index'
 
     id_q = aux['identifier'].unique()
+
+    df_total = pd.DataFrame(index=aux['ID_R'].unique())
+    df_total['ID_R'] = df_total.index
+    df_total = df_total.reset_index(drop=True)
     for identifier in id_q:
         tmp = aux[aux['identifier'] == identifier].copy()
-        tmp = tmp[['ID', 'identifier', 'value']]
-        tmp = tmp.set_index('ID')
+        tmp = tmp[['ID_R', 'identifier', 'value']]
+        tmp = tmp.set_index('ID_R')
         tmp = tmp.pivot(columns='identifier', values='value')
 
+        df_total = pd.merge(left=df_total,
+                            right=tmp,
+                            how='inner',
+                            on='ID_R')
+
     # Revert back the columns
+    df_placeholder = pd.merge(left=df_placeholder,
+                              right=df_total,
+                              how='inner',
+                              on='ID_R')
 
     t1 = time.time()
     if verbose:
         print('Constructing diffs took: {:6.1f} sec'.format(t1 - t0))
-    return df
+
+    return df_placeholder
 # ===========================================================================

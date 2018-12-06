b7c5fea1e2210d139643dc255ef85733f0c72603
AndPotap <ap3635@columbia.edu>
2018-12-06 00:34:05 -0500

    Added the data from all the years

--- AndPotap/distances_analysis.py
+++ AndPotap/distances_analysis.py
@@ -32,6 +32,10 @@ file_input = './DBs/marathon_2018.csv'
 file_output = './DBs/marathon_2018_clusters.csv'
 # file_input = './DBs/marathon_2017.csv'
 # file_output = './DBs/marathon_2017_clusters.csv'
+# file_input = './DBs/marathon_2016.csv'
+# file_output = './DBs/marathon_2016_clusters.csv'
+# file_input = './DBs/marathon_2015.csv'
+# file_output = './DBs/marathon_2015_clusters.csv'
 # ===========================================================================
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Load the data
@@ -39,21 +43,20 @@ file_output = './DBs/marathon_2018_clusters.csv'
 # data = pd.read_csv(file_input, nrows=100)  # Read a chunk
 data = pd.read_csv(file_input)  # Read all
 data = data[data['type'] == 'R']
-mask = 4000
-df = data.loc[:mask, :].copy()
+data = format_correctly(data, verbose=True)
+data = add_diffs(data, verbose=True)
+np.random.seed(seed=37373)
+# data = data[data['sec_8'] < 15000]
+mask = 10000
+mask = np.random.choice(a=data.shape[0], size=mask)
+df = data.reindex(mask).copy()
 # ===========================================================================
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Format dates into the correct shape
 # ===========================================================================
-df = format_correctly(df, verbose=True)
-df = add_diffs(df, verbose=True)
 df_out = df.copy()
-all_cols = [col for col in df.columns if 'diff' in col]
-# mask = pd.notna(df_out[all_cols])
-# df_out = df_out.loc[mask, :]
-# x = df_out['sec_8'].values
-# x = x.reshape((x.shape[0], 1))
 df_out = df_out.dropna()
+all_cols = [col for col in df.columns if 'diff' in col]
 x = df_out[all_cols].values
 N = x.shape[0]
 t0 = time.time()
@@ -64,7 +67,6 @@ print(f'\nDistance matrix: {time.time() - t0:6.1f} sec')
 # ===========================================================================
 # Optimize
 # ===========================================================================
-np.random.seed(seed=37373)
 x0 = np.random.uniform(low=-100, high=100, size=(N, 2))
 pool = multiprocessing.Pool()
 kwargs_f = {'d': d, 'pool': pool}
@@ -76,6 +78,7 @@ x_min, i = sgd_all(func=discrepancy_map,
                    x0=x0,
                    eta=1.e-3,
                    batch_pct=1,
+                   max_iter=500,
                    variant='SGD',
                    size=N,
                    return_vec=False,
@@ -95,5 +98,5 @@ plt.show()
 # ===========================================================================
 df_out.loc[:, 'x'] = x_min[:, 0]
 df_out.loc[:, 'y'] = x_min[:, 1]
-df_out.to_csv(file_output, index=False)
+df_out.to_csv(file_output)
 # ===========================================================================
